109fb00b30998447598749ff8de982a538978bfb
Aurelie75FR <aurelielo.vi@gmail.com>
2021-04-27 18:45:58 +0200

    only one bug and start to make the code shorter

--- gameJs/index.js
+++ gameJs/index.js
@@ -67,14 +67,14 @@ function select(i, team) {
 }
 // the fun part : the card placement
 function cardsPlacement(nb) {
-  if (empty(nb)) {
-    if (playerTurn === "blue") {
-      if (typeof (blueCard) === "undefined") {
+  if (emptyCell(nb)) {
+    if (playerTurn == "blue") {
+      if (typeof (blueCard) == "undefined") {
         alert("You didn't select any card !");
       } else {
         document.getElementById(nb).src = blueCard.img;
-        document.getElementById(nb).style.border = '10px solid #49D4DB';
-        document.getElementById(nb).style.borderRadius = "12px";
+        document.getElementById(nb).style.border = '5px solid blue';
+        document.getElementById(nb).style.borderRadius = "10px";
         blackNwhiteCard(blueCard);
         cardPlay[nb] = blueCard;
         endOfTurn(nb);
@@ -84,13 +84,13 @@ function cardsPlacement(nb) {
         playerTurn = "red";
       }
     } else {
-      if (typeof (redCard) === "undefined") {
+      if (typeof (redCard) == "undefined") {
         alert("you didn't select any card !");
       } else {
         document.getElementById(nb).src = redCard.img;
-        document.getElementById(nb).style.border = '10px solid #EC201A';
-        document.getElementById(nb).style.borderRadius = "12px";
-        blackNwhiteCard(redCard);
+        document.getElementById(nb).style.border = '5px solid #EC201A';
+        document.getElementById(nb).style.borderRadius = "10px";
+       blackNwhiteCard(redCard);
         cardPlay[nb] = redCard;
         endOfTurn(nb);
         document.getElementById("nextTurn").innerHTML =
@@ -101,13 +101,8 @@ function cardsPlacement(nb) {
     }
   } else {
     alert("A card is already place here!");
-    console.log(cardPlay);
   }
-  console.log(cardPlay);
-  console.log(endOfTurn());
-  console.log(blackNwhiteCard());
-  console.log(blueCard);
-  console.log(empty());
+ 
   console.log(playerTurn);
 }
 
@@ -115,23 +110,22 @@ function endOfTurn(nb) {
   turnVerification(nb);
   document.getElementById("cardSelect").innerHTML =
     'Pick a card : <img src="img/back.png">';
-    // `Pick a card : <img src="img/back.png>`
   nbTurn++;
   whoWon();
 }
 //Behold people! Here we are with Regex
-function empty(nb) {
+function emptyCell(nb) {
   let regex = /\w*.png/; //<-- from MDN it's replace() String x name of the card the player pick
-  if (document.getElementById(nb).src.match(regex) === "back.png") {
+  if (document.getElementById(nb).src.match(regex) == "back.png") {
     return true;
   } else {
     return false;
   }
 }
 
-function occupy(nb) {
+function occupyCell(nb) {
   let regex = /\w*.png/;
-  if (document.getElementById(nb).src.match(regex) != "back.png") {
+  if (document.getElementById(nb).src.match(regex) != 'back.png') {
     return true;
   } else {
     return false;
@@ -139,186 +133,190 @@ function occupy(nb) {
 }
 
 function turnVerification(nb) {
-  let color = "red";
-  if (playerTurn === "blue") {
-    color = "#49D4DB";
+  let color = 'red';
+  if (playerTurn == 'blue') {
+    color = 'blue';
   }
   //let's compare cards value on the grid
-  let border = "10px solid " + color;
+  let border = '5px solid ' +color;
   switch (nb) {
     case 1: //<-- corner up left
-      if (occupy(2)) {
-        if (cardPlay[1].rightValue > cardPlay[2].leftValue) {
-          document.getElementById(2).style.border = border;
+      if (occupyCell(2)) {
+        if (cardPlay[1].rightValue>cardPlay[2].leftValue) {
+          document.getElementById(2).style.border=border
         }
       }
-      if (occupy(4)) {
-        if (cardPlay[1].downValue > cardPlay[4].upValue) {
-          document.getElementById(4).style.border = border;
+      if (occupyCell(4)) {
+        if (cardPlay[1].downValue>cardPlay[4].upValue) {
+          document.getElementById(4).style.border=border
         }
       }
       break;
 
     case 2: // <-- up middle
-      if (occupy(1)) {
-        if (cardPlay[2].leftValue > cardPlay[1].rightValue) {
-          document.getElementById(1).style.border = border;
+      if (occupyCell(1)) {
+        if (cardPlay[2].leftValue>cardPlay[1].rightValue) {
+          document.getElementById(1).style.border=border
         }
       }
-      if (occupy(3)) {
-        if (cardPlay[2].rightValue > cardPlay[3].leftValue) {
-          document.getElementById(3).style.border = border;
+      if (occupyCell(3)) {
+        if (cardPlay[2].rightValue>cardPlay[3].leftValue) {
+          document.getElementById(3).style.border=border
         }
       }
-      if (occupy(5)) {
-        if (cardPlay[2].downValue > cardPlay[5].upValue) {
-          document.getElementById(5).style.border = border;
+      if (occupyCell(5)) {
+        if (cardPlay[2].downValue>cardPlay[5].upValue) {
+          document.getElementById(5).style.border=border
         }
       }
       break;
 
     case 3: //<-- corner up right
-      if (occupy(2)) {
-        if (cardPlay[3].leftValue > cardPlay[2].rightValue) {
-          document.getElementById(2).style.border = border;
+      if (occupyCell(2)) {
+        if (cardPlay[3].leftValue>cardPlay[2].rightValue) {
+          document.getElementById(2).style.border=border
         }
       }
-      if (occupy(6)) {
-        if (cardPlay[3].downValue > cardPlay[6].upValue) {
-          document.getElementById(6).style.border = border;
+      if (occupyCell(6)) {
+        if (cardPlay[3].downValue>cardPlay[6].upValue) {
+          document.getElementById(6).style.border=border
         }
       }
       break;
 
     case 4: //<-- middle left
-      if (occupy(1)) {
-        if (cardPlay[4].upValue > cardsPlay[1].downValue) {
-          document.getElementById(1).style.border = border;
+      if (occupyCell(1)) {
+        if (cardPlay[4].upValue>cardPlay[1].downValue) {
+          document.getElementById(1).style.border=border
         }
       }
-      if (occupy(5)) {
-        if (cardsPlay[4].rightValue > cardPlay[5].leftValue) {
-          document.getElementById(5).style.border = border;
+      if (occupyCell(5)) {
+        if (cardPlay[4].rightValue>cardPlay[5].leftValue) {
+          document.getElementById(5).style.border=border
         }
       }
-      if (occupy(7)) {
-        if (cardPlay[4].downValue > cardPlay[7].upValue) {
-          document.getElementById(7).style.border = border;
+      if (occupyCell(7)) {
+        if (cardPlay[4].downValue>cardPlay[7].upValue) {
+          document.getElementById(7).style.border=border
         }
       }
       break;
 
     case 5: //<-- middle
-      if (occupy(2)) {
-        if (cardPlay[5].upValue > cardPlay[2].downValue) {
-          document.getElementById(2).style.border = border;
+      if (occupyCell(2)) {
+        if (cardPlay[5].upValue>cardPlay[2].downValue) {
+          document.getElementById(2).style.border=border
         }
       }
-      if (occupy(4)) {
-        if (cardPlay[5].leftValue > cardPlay[4].rightValue) {
-          document.getElementById(4).style.border = border;
+      if (occupyCell(4)) {
+        if (cardPlay[5].leftValue>cardPlay[4].rightValue) {
+          document.getElementById(4).style.border=border
         }
       }
-      if (occupy(6)) {
-        if (cardPlay[5].rightValue > cardPlay[6].leftValue) {
-          document.getElementById(6).style.border = border;
+      if (occupyCell(6)) {
+        if (cardPlay[5].rightValue>cardPlay[6].leftValue) {
+          document.getElementById(6).style.border=border
         }
       }
-      if (occupy(8)) {
-        if (cardPlay[5].downValue > cardPlay[8].upValue) {
-          document.getElementById(8).style.border = border;
-        }
-      }
-
-    case 6: //<-- middle right
-      if (occupy(3)) {
-        if (cardPlay[6].upValue > cardPlay[3].downValue) {
-          document.getElementById(3).style.border = border;
-        }
-      }
-      if (occupy(5)) {
-        if (cardPlay[6].leftValue > cardPlay[5].rightValue) {
-          document.getElementById(5).style.border = border;
-        }
-      }
-      if (occupy(9)) {
-        if (cardPlay[6].downValue > cardPlay[9].upValue) {
-          document.getElementById(9).style.border = border;
+      if (occupyCell(8)) {
+        if (cardPlay[5].downValue>cardPlay[8].upValue) {
+          document.getElementById(8).style.border=border
         }
       }
       break;
-    case 7: //<-- corner down left
-      if (occupy(4)) {
-        if (cardPlay[7].upValue > cardPlay[4].downValue) {
-          document.getElementById(4).style.border = border;
+
+    case 6: //<-- middle right
+      if (occupyCell(3)) {
+        if (cardPlay[6].upValue>cardPlay[3].downValue) {
+          document.getElementById(3).style.border=border
         }
       }
-      if (occupy(8)) {
-        if (cardPlay[7].rightValue > cardPlay[8].leftValue) {
-          document.getElementById(8).style.border = border;
+      if (occupyCell(5)) {
+        if (cardPlay[6].leftValue>cardPlay[5].rightValue) {
+          document.getElementById(5).style.border=border
+        }
+      }
+      if (occupyCell(9)) {
+        if (cardPlay[6].downValue>cardPlay[9].upValue) {
+          document.getElementById(9).style.border=border
+        }
+      }
+      break;
+
+    case 7: //<-- corner down left
+      if (occupyCell(4)) {
+        if (cardPlay[7].upValue>cardPlay[4].downValue) {
+          document.getElementById(4).style.border=border
+        }
+      }
+      if (occupyCell(8)) {
+        if (cardPlay[7].rightValue>cardPlay[8].leftValue) {
+          document.getElementById(8).style.border=border
         }
       }
       break;
 
     case 8: //<-- down middle
-      if (occupy(5)) {
-        if (cardPlay[8].upValue > cardPlay[5].downValue) {
-          document.getElementById(5).style.border = border;
+      if (occupyCell(5)) {
+        if (cardPlay[8].upValue>cardPlay[5].downValue) {
+          document.getElementById(5).style.border=border
         }
       }
-      if (occupy(7)) {
-        if (cardPlay[8].leftValue > cardPlay[7].rightValue) {
-          document.getElementById(7).style.border = border;
+      if (occupyCell(7)) {
+        if (cardPlay[8].leftValue>cardPlay[7].rightValue) {
+          document.getElementById(7).style.border=border
         }
       }
-      if (occupy(9)) {
-        if (cardPlay[8].rightValue > cardPlay[9].leftValue) {
-          document.getElementById(9).style.border = border;
+      if (occupyCell(9)) {
+        if (cardPlay[8].rightValue>cardPlay[9].leftValue) {
+          document.getElementById(9).style.border=border
         }
       }
       break;
 
     case 9:
-      if (occupy(6)) {
-        if (cardPlay[9].upValue > cardPlay[6].downValue) {
-          document.getElementById(6).style.border = border;
+      if (occupyCell(6)) {
+        if (cardPlay[9].upValue>cardPlay[6].downValue) {
+          document.getElementById(6).style.border=border
         }
       }
-      if (occupy(8)) {
-        if (cardPlay[9].leftValue > cardPlay[8].rightValue) {
-          document.getElementById(8).style.border = border;
-        }
+      if (occupyCell(8)) {
+        if (cardPlay[9].leftValue>cardPlay[8].rightValue) {
+          document.getElementById(8).style.border=border
+        } 
       }
       break;
   }
 }
-//lost card so they turn black and white
-function blackNwhiteCard(card) {
+//Cards already played so they turn black and white
+function blackNwhiteCard(cards) {
   color = "red";
-  if (playerTurn === "blue") {
+  if (playerTurn == "blue") {
     color = "blue";
   }
-  document.getElementById(color + cards.img).style.filter = "grayscale(100%)";
-  document.getElementById(color + cards.img).onclick = function () {};
+  document.getElementById(color + cards.img).style.filter = 'grayscale(100%)';
+  document.getElementById(color + cards.img).onclick = function(){};
 }
 
 function whoWon() {
-  if (nbTurn === 9) {
-    let scoreB = 0;
-    let scoreR = 0;
-    for (let i = 0; i < 10; i++) {
-      let regex = /\w*$/; //any word character to the end of a string(global search)
-      if (document.getElementById(i).style.border.match(regex) == "blue") {
-        scoreB++;
-      } else {
-        scoreR++;
+  if (nbTurn==9) {
+    let scoreB = 0
+    let scoreR = 0
+    for (let i = 1; i < 10; i++) {
+      var regex = /\w*$/
+      if (document.getElementById(i).style.border.match(regex)=='blue') {
+        scoreB++
+      }
+      else {
+        scoreR++
       }
     }
-    if (scoreB > scoreR) {
-      alert("Congrats!! Blue win! Play Again ?");
-    } else {
-      alert("Congrats!! Red win! Play Again ?");
+    if (scoreR>scoreB) {
+      alert('Red win! Play Again?')
     }
-    window.location.reload();
+    else {
+      alert('Blue win! Play Again?')
+    }
+    window.location.reload()
   }
 }
